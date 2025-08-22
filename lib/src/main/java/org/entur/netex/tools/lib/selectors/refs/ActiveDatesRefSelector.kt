d7de28aed58c5955fdb57726406e7b0d949a0de3
erlendev <erlend.vige@gmail.com>
2025-08-22 10:01:35 +0200

    Adds checks of OperatingPeriod+Day in ActiveDatesRefSelector

--- lib/src/main/java/org/entur/netex/tools/lib/selectors/refs/ActiveDatesRefSelector.kt
+++ lib/src/main/java/org/entur/netex/tools/lib/selectors/refs/ActiveDatesRefSelector.kt
@@ -4,6 +4,7 @@ import org.entur.netex.tools.lib.config.TimePeriod
 import org.entur.netex.tools.lib.model.EntityModel
 import org.entur.netex.tools.lib.plugin.activedates.ActiveDatesPlugin
 import org.entur.netex.tools.lib.plugin.activedates.data.DayTypeData
+import org.entur.netex.tools.lib.plugin.activedates.model.Period
 import org.entur.netex.tools.lib.selections.RefSelection
 import java.time.LocalDate
 
@@ -12,6 +13,13 @@ class ActiveDatesRefSelector(val activeDatesPlugin: ActiveDatesPlugin, val perio
     private fun isDateInPeriod(date: LocalDate, dayOffset: Long, startDate: LocalDate, endDate: LocalDate): Boolean {
         val adjustedDate = date.plusDays(dayOffset)
         return !date.isAfter(endDate) && !adjustedDate.isBefore(startDate)
+    }
+
+    private fun isPeriodOverlapping(period: Period, dayOffset: Long, startDate: LocalDate, endDate: LocalDate): Boolean {
+        val fromDate = period.fromDate ?: return false
+        val toDate = period.toDate ?: return false
+
+        return !fromDate.isAfter(endDate) && !toDate.plusDays(dayOffset).isBefore(startDate)
     }
 
     override fun selectRefs(model: EntityModel): RefSelection {
@@ -27,7 +35,15 @@ class ActiveDatesRefSelector(val activeDatesPlugin: ActiveDatesPlugin, val perio
                 val datesWithinPeriod = dayTypeData.dates.filter {
                     isDateInPeriod(it, serviceJourneyData.finalArrivalDayOffset, period.start, period.end)
                 }
-                if (datesWithinPeriod.isEmpty()) {
+                val operatingDaysWithinPeriod = dayTypeData.operatingDays.filter {
+                    val operatingDay = repository.operatingDays[it] ?: return@filter false
+                    isDateInPeriod(operatingDay, serviceJourneyData.finalArrivalDayOffset, period.start, period.end)
+                }
+                val operatingPeriodsWithinPeriod = dayTypeData.operatingPeriods.filter { operatingPeriodRef ->
+                    val operatingPeriod = repository.operatingPeriods[operatingPeriodRef]?.period ?: return@filter false
+                    isPeriodOverlapping(operatingPeriod, serviceJourneyData.finalArrivalDayOffset, period.start, period.end)
+                }
+                if (datesWithinPeriod.isEmpty() && operatingDaysWithinPeriod.isEmpty() && operatingPeriodsWithinPeriod.isEmpty()) {
                     val refToRemove = model.getRefOfTypeFromSourceIdAndRef(
                         sourceId = serviceJourneyId,
                         ref = dayTypeRef,
